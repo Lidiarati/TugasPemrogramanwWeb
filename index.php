7c60005528923a9bee1065dd4a476393a3a6e4eb
Lidiarati <81345277+Lidiarati@users.noreply.github.com>
2022-03-16 23:36:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include 'functions.php';
 
 <div class="content">
 	<h2>Home</h2>
-	<p>Selamat datang di halaman Home rati</p>
+	<p>Selamat datang di halaman Home rati edit</p>
 </div>
 
 <?=template_footer()?>
